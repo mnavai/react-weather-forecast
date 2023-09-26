c31e833b037f844c0a109c7fd6fb2252197c5bd2
Maryam Navaei <maryam.navaei@gmail.com>
2023-09-26 11:26:15 -0400

    adding DropDown component

--- src/component/DropDown/DropDown.jsx
+++ src/component/DropDown/DropDown.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import './DropDown.css';
+import "./DropDown.css";
 
-const DropDown = (className, name, onClick, text) => {
-    return (
-      <div className="drop-down">
-        <select className={className} name={name} onClick={onClick}>{text}</select>
-      </div>
-    );
-}
+const DropDown = (className, name, onClick, children) => {
+  return (
+    <div className="drop-down">
+      <select className={className} name={name} onClick={onClick}>
+        {children}
+      </select>
+    </div>
+  );
+};
 export default DropDown;
