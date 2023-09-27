f836021874c06e967377141fe63e0fd790162e6a
Maryam Navaei <maryam.navaei@gmail.com>
2023-09-27 16:23:32 -0400

    working on Card component

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./Card.css";
-const Card = () => {
+
+const Card = (date, src, alt, status, high, low) => {
   return (
     <div className="card-container">
-
+      <h1 className="card-date">{date}</h1>
+      <img src={src} alt={alt}></img>
+      <h3 className="card-weather-status">{status}</h3>
+      <h5 className="high">{high}</h5>
+      <h5 className="low">{low}</h5>
     </div>
   )
 };
