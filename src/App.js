52c9ce8f9f99ca4fc1ddb01e252abae37bf23b2e
Maryam Navaei <maryam.navaei@gmail.com>
2023-10-11 12:09:48 -0400

    added XML parser

--- src/App.js
+++ src/App.js
@@ -17,20 +17,65 @@ function App() {
   //fetching successfully done
   useEffect(() => {
     const handleOnClick = async () => {
+      function xmlToJson(xml) {
+        // Create the return object
+        let obj = {};
+
+        if (xml.nodeType === 1) {
+          // element
+          // do attributes
+          if (xml.attributes.length > 0) {
+            obj["@attributes"] = {};
+            for (let j = 0; j < xml.attributes.length; j++) {
+              const attribute = xml.attributes.item(j);
+              obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+            }
+          }
+        } else if (xml.nodeType === 3) {
+          // text
+          obj = xml.nodeValue;
+        }
+
+        // do children
+        if (xml.hasChildNodes()) {
+          for (let i = 0; i < xml.childNodes.length; i++) {
+            const item = xml.childNodes.item(i);
+            const nodeName = item.nodeName;
+            if (typeof obj[nodeName] === "undefined") {
+              obj[nodeName] = xmlToJson(item);
+            } else {
+              if (typeof obj[nodeName].push === "undefined") {
+                const old = obj[nodeName];
+                obj[nodeName] = [];
+                obj[nodeName].push(old);
+              }
+              obj[nodeName].push(xmlToJson(item));
+            }
+          }
+        }
+        return obj;
+      }
+
       if (selectedCity) {
         try {
           const response = await fetch(
-            `https://www.7timer.info/bin/api.pl?lon=${selectedCity.lon}&lat=${selectedCity.lat}&product=astro&output=json`
+            `http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=civillight&output=xml`
           );
 
           if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
           } else {
-            const data = await response.json();
-            console.log("data:", data);
-            setForecastData(data);
-            console.log("forecast data is", data)
+
+            const data = await response.text();
+            const parser = new DOMParser()
+            const xmlDoc = parser.parseFromString(data, 'application/xml')
+            const jsonData = xmlToJson(xmlDoc)
+            console.log("data:", jsonData);
+            setForecastData(jsonData);
+            console.log("forecast data is", jsonData)
             setLoading(false);
+
+
           }
         } catch (error) {
           console.error("Error fetching data from API", error);
