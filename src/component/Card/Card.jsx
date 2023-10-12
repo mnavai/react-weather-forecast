d674b7f005cb6411617c3c36e61934cc87f5640f
Maryam Navaei <maryam.navaei@gmail.com>
2023-10-11 23:11:12 -0400

    latest updates

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ className,weather, high, low }) => {
   return (
     <div className={className}>
       <h1 className="card-date">{weather.description}</h1>
-      <h3 className="card-weather-status">{weather.description}</h3>
+      <h3 className="card-weather-status">{weather["#text"]}</h3>
       <h5 className="high">High {high["#text"]}</h5>
       <h5 className="low">Low {low["#text"]}</h5>
     </div>
